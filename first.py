f7ff8b0aec637837775f04a8a23a1b6f28eb3935
jondo201010 <37029062+jondo201010@users.noreply.github.com>
2018-06-17 21:12:54 +1000

    Add files via upload

---
+++ first.py
@@ -0,0 +1,40 @@
+
+
+from plotly import __version__
+from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
+import plotly.graph_objs as go
+
+import numpy as np
+import pandas as pd
+init_notebook_mode(connected=True)
+x = np.random.randn(2000)
+y = np.random.randn(2000)
+fullData=pd.read_csv("C://amit//Learn//Python//GitMcLearningCourseraCode//ipython-notebooks//data//ex1data1.txt", names=['Population','Profit'],header=None)
+print(fullData.columns)
+print(fullData.head(10))
+
+layout_comp = go.Layout(
+    title='Life Expectancy v. Per Capita GDP, 2007',
+    hovermode='closest',
+    xaxis=dict(
+        title='GDP per capita (2000 dollars)',
+        ticklen=5,
+        zeroline=False,
+        gridwidth=2,
+    ),
+    yaxis=dict(
+        title='Life Expectancy (years)',
+        ticklen=5,
+        gridwidth=2,
+    ),
+)
+
+#trsce=go.Figure(data=[go.Scatter(x=fullData['Population'], y=fullData['Profit'])], layout=layout_comp)
+#plot(go.Figure(data=[trsce],layout=layout_comp))
+
+plot(fullData['Population'],fullData['Profit'],'go')
+#plot([go.Scatter(x=[1, 2, 3], y=[3, 1, 6])])
+#plot([go.Scatter(x=fullData['Population'], y=fullData['Profit'])])
+
+#plot([go.Histogram2dContour(x=x, y=y, contours=dict(coloring='heatmap')),
+ #      go.Scatter(x=x, y=y, mode='markers', marker=dict(color='white', size=3, opacity=0.3))], show_link=False)
---
+++ s.py
@@ -0,0 +1,57 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import pandas as pd
+import math
+
+
+class Line:
+    def __init__(self, x1,y1,x2,y2):
+        self.slope=(y2-y1)/(x2-x1)
+        self.y_intercept=y2-self.slope*x2
+
+    def getSlope():
+        return self.slope
+
+    def getIntercept():
+        return self.y_intercept
+
+    def getDistance(self,x,y):
+        dist=abs(self.y_intercept+self.slope*x-y)/(math.sqrt(1+self.slope**2))
+        return dist
+
+
+
+fullData=pd.read_csv("C://amit//Learn//Python//GitMcLearningCourseraCode//ipython-notebooks//data//ex1data1.txt", names=['Population','Profit'],header=None)
+
+#plt.plot(x=fullData['Population'], y=fullData['Profit'])
+
+plt.scatter(fullData['Population'],  fullData['Profit'])
+#plt.plot([10, 20], [5, 20], 'k-', lw=2)
+x=fullData['Population']
+y=fullData['Profit']
+
+
+# To start get Two Points
+# First Point
+# average of x and y
+initialGuessLine_Point1_x=x.mean()
+
+initialGuessLine_Point1_y =y.mean()
+
+
+# point 2
+# Max(x)-Min(x),Max(y)-Min(y)
+initialGuessLine_Point2_x=(x.max()+x.min())/2
+
+initialGuessLine_Point2_y =(y.max()+y.min())/2
+print(initialGuessLine_Point1_x,initialGuessLine_Point1_y)
+print(initialGuessLine_Point2_x,initialGuessLine_Point2_y)
+print(x.max(),x.min())
+plt.plot([initialGuessLine_Point1_x, initialGuessLine_Point2_x], [initialGuessLine_Point1_y, initialGuessLine_Point2_y], 'k-', lw=2)
+
+ln= Line(initialGuessLine_Point1_x,initialGuessLine_Point1_y,initialGuessLine_Point2_x,initialGuessLine_Point2_y)
+print(ln.getDistance(10,13))
+plt.show()
+
+
