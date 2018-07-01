a6eb0860ae2141b92e0703bff594f985a9be2dd6
t <jondo201010@gmail.com>
2018-07-01 20:43:35 +1000

    Added a test file

---
+++ Line.py
@@ -0,0 +1,17 @@
+import math
+
+
+class Line:
+    def __init__(self, x1,y1,x2,y2):
+        self.slope=(y2-y1)/(x2-x1)
+        self.y_intercept=y2-self.slope*x2
+
+    def getSlope(self):
+        return self.slope
+
+    def getIntercept(self):
+        return self.y_intercept
+
+    def getDistance(self,x,y):
+        dist=abs(self.y_intercept+self.slope*x-y)/(math.sqrt(1+self.slope**2))
+        return dist
